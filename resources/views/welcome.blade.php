b260b7657e7c4d3a8603d7ddda5e68ecafd27644
chernanat <chernan1234@hotmail.com>
2022-06-06 16:28:09 -0500

    Node Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
+        <link rel="stylesheet" href="{{mix('css/app.css')}}">
         <!-- Styles -->
         <style>
 
@@ -20,5 +20,19 @@
     </head>
     <body class="antialiased">
 
+        <ul class="nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
     </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix 
+    .js('resources/js/app.js','public/js').vue()
+    .sass('resources/sass/app.scss','public/css')
+    .webpackConfig(require('./webpack.config'));
+
+mix.version();
+mix.disableSuccessNotifications();
+
